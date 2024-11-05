06970e4b47cbd1e8a239d4803e216bb6a085de10
Anpidev <angelaruizdev@gmail.com>
2024-11-05 08:44:59 +0100

    DDL_DML_DQL_DCL

---
+++ DML/INSERT/pais.sql
@@ -0,0 +1,14 @@
+
+
+INSERT INTO CentroMedico.dbo.Pais (idPais,pais)
+	VALUES (N'ESP',N'España');
+INSERT INTO CentroMedico.dbo.Pais (idPais,pais)
+	VALUES (N'ARG',N'Argentina');
+INSERT INTO CentroMedico.dbo.Pais (idPais,pais)
+	VALUES (N'BRA',N'Brasil');
+INSERT INTO CentroMedico.dbo.Pais (idPais,pais)
+	VALUES (N'COL',N'Colombia');
+INSERT INTO CentroMedico.dbo.Pais (idPais,pais)
+	VALUES (N'MEX',N'Mexico');
+INSERT INTO CentroMedico.dbo.Pais (idPais,pais)
+	VALUES (N'PER',N'Peru');
